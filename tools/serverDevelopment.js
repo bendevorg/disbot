8cf976e5419e3bd2189375fc7400d4840549a729
GuiMac <guibasconti@gmail.com>
2017-11-08 20:47:05 -0200

    Placeholder discord setup

--- tools/serverDevelopment.js
+++ tools/serverDevelopment.js
@@ -1,6 +1,6 @@
 'use strict';
 /* eslint-disable no-console */
-const dotenv = require('dotenv');
+/**const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
@@ -14,3 +14,19 @@ app.use(logger.errorHandler());
 app.use(morgan('tiny'));
 
 module.exports = app;
+**/
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('token');
