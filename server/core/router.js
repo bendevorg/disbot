0b8dd7e6f154976a49bc3be9383eed60fd233a73
GuiMac <guibasconti@gmail.com>
2017-11-11 08:30:31 -0200

    Change play file route and add play stream route'

--- server/core/router.js
+++ server/core/router.js
@@ -5,12 +5,14 @@ const router = express.Router();
 router.use(bodyParser.json());
 
 const playAudio = require('../controllers/playAudio');
+const playStream = require('../controllers/playStream');
 
 //  Placeholder API
 router.get('/', (req, res) => {
   res.status(200).json({msg: 'Hi!'});
 });
 
-router.get('/play/:audioId', playAudio);
+router.get('/play/file/:audioId', playAudio);
+router.get('/play/stream', playStream);
 
 module.exports = router;
